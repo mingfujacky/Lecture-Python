9bf19fc88c6905d478346dcee9877744421c3437
Jacky Lin <jacky.jw.lin@nycu.edu.tw>
2024-12-26 17:01:55 +0800

    revise matplotlib material

--- utils/goodbye/greeting.py
+++ utils/goodbye/greeting.py
@@ -1,17 +1,6 @@
-ID = "D00073"
-
-
-def hello():
-    print("Hello, World!")
-
-
 def goodbye():
-    print("Goodbye, World!")
+    print("Goodbye!")
 
 
-def hi():
-    print("Hi, World!")
-
-
-if __name__ == "__main__":
-    print("greeting.py is loaded")
+def seeyou():
+    print("See you!")
