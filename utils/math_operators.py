142ce4a33a590e3823c9d2c0a07dd4277fbad745
Jacky Lin <jacky.jw.lin@gmail.com>
2025-01-03 16:41:19 +0800

    revise module  material

---
+++ utils/math_operators.py
@@ -0,0 +1,23 @@
+'''
+math_operators.py 提供數學的四則運算函數
+parameters : 兩個整數
+return: 計算後結果
+'''
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+if __name__ == '__main__':
+    print(add(1, 2))
+    print(sub(1, 2))
+    print(mul(1, 2))
+    print(div(1, 2))
