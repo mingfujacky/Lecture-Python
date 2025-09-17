6755675ee00ecd2b437cf3623030029dab503c6b
Jacky Lin <jacky.jw.lin@nycu.edu.tw>
2025-09-17 17:33:11 +0800

    revise middle K L

--- 1_python_basic/class_demo.py
+++ 1_python_basic/class_demo.py
@@ -1,15 +1,6 @@
-'''
-九九乘法表
-寫一個python程式, 輸入一個句子，然後計算該句子內的字數，還有最長的字是哪一個
-'''
-sentence = input("請輸入一個句子: ")
-words = sentence.split()
-word_count = len(words)
-longest_word = max(words, key=len)
-
-print(f"字數: {word_count}")
-print(f"最長的字: {longest_word}")
-
-for i in range(1, 10):
-    for j in range(1, 10):
-        print(f"{i} * {j} = {i * j}")
+a = 1
+b = 3
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
